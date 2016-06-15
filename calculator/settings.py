c1d0df87b6b62a503918a6eb668570dea9c669fa
ramesh7128 <ramesh7128@gmail.com>
2016-06-16 02:21:34 +0530

    added static file settings

--- calculator/settings.py
+++ calculator/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 
@@ -139,6 +138,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 # STATIC_URL = '/static/'
+
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
